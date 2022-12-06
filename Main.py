198f68a3d693b03a1795e0c96b2edb6ef7017720
Shiwei Ge <ajie4095@gmail.com>
2022-12-06 13:59:57 -0500

    finish real time

--- Main.py
+++ Main.py
@@ -4,24 +4,29 @@ from FaceDetector import FaceDetector
 from FaceSegmentation import FaceSegmentation
 from SegmentSwapping import SegmentSwapper
 from FaceSwapping import FaceSwaper
+import numpy as np
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('i', None, 'Input Image (first image path)')
 flags.DEFINE_string('o', None, 'Swap the face to (second image path)')
+flags.DEFINE_string('video', None, "Swap the face to the video")
 
 class Main:
 
     @staticmethod
     def main(argv):
         del argv
-        if FLAGS.i == None or FLAGS.o == None:
-            raise RuntimeError('Images are not correctly specified!')
+        if FLAGS.i == None or (FLAGS.o == None and FLAGS.video == None):
+            raise RuntimeError('Images or videos are not correctly specified!')
         
-        Main.run(FLAGS.i, FLAGS.o)
+        Main.run(FLAGS.i, FLAGS.o, FLAGS.video)
 
     @staticmethod
-    def run(img_path_from, img_path_to):
-        Main.run_pipeline(img_path_from, img_path_to)
+    def run(img_path_from, img_path_to, video_path_to):
+        if img_path_to:
+            Main.run_pipeline(img_path_from, img_path_to)
+        else:
+            Main.run_pipeline_video(img_path_from, video_path_to)
 
     @staticmethod
     def run_pipeline(img_path_from, img_path_to):
@@ -46,7 +51,41 @@ class Main:
         
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+        
+    @staticmethod
+    def run_pipeline_video(img_path_from, video_path_to):
+        # capture the video
+        cap = cv2.VideoCapture(video_path_to)
+        # Step 1: Detect for face and get landmarks from the first image
+        print(img_path_from, video_path_to)
+        img1, landmarks1, polygon1, _, _ = FaceDetector.get_img_landmarks_polygon_mask_and_face(
+            img_path_from)
+             
+        while True:
+            check, img2 = cap.read()
+            if not check:
+                break
+            img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)      
+            # Step 2: Detect for face and get landmarks from the second image
+            face_detector_2 = FaceDetector(img2,img2_gray)
+            landmarks2 = face_detector_2.get_landmarks()[0]
+            points = np.array(landmarks2, np.int32)
+            polygon2 = face_detector_2.get_convex_polygon(points)
+            
+            # Step 3: Find the corrisbonding triangles between two images 
+            triangles1, triangles2 = FaceSegmentation.get_corrisbonding_triangles(landmarks1, polygon1, landmarks2)
+            
+            # Step 4: Generate the new face from first image
+            new_face = SegmentSwapper.get_new_face(img1, img2, triangles1, triangles2)
+            
+            # Step 5: Put the generated face area to the second image
+            result = FaceSwaper.get_swapped_res(img2, img2_gray, polygon2, new_face)
 
+            cv2.imshow("Result", result)
+            
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
 
 if __name__ == '__main__':
     app.run(Main.main)
